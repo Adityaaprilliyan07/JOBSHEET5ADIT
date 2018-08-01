8037921b3507d401236a58eff98ad76a09290775
Asus <Asus@DESKTOP-R7PQKTL.mshome.net>
2018-08-01 08:09:44 +0700

    KEDUA

---
+++ src/Latihan/OperatorAritmatika.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Asus
+ */
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        
+        int c;
+        
+        //penjumlahan
+        c = a + b;
+        System.out.println(a + " + " + b + " = " + c);
+        //kurang
+        c = a - b;
+        System.out.println(a + " - " + b + " = "+ c);
+        //kali
+        c = a * b;
+        System.out.println(a + " x " + b + " = " + c);
+        //bagi
+        c = a / b;
+        System.out.println(a + " : " + b + " = " + c);
+        //sisa hasil bagi
+        c = a % b;
+        System.out.println(a + " % " + b + " = " + c);
+    }
+}
+        
+        
+        
+        
+        
+    
+
